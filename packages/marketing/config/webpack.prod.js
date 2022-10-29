fa86d1452601b6927a21bd771f55506a9995a100
Joaquim Ignatius Llamas <joaquimignatius.llamas@softvision.com>
2022-10-29 15:35:43 +0800

    contenhash to hash

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,7 @@ const packageJson = require("../package.json");
 const prodConfig = {
   mode: "production",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "[name].[hash].js",
     publicPath: "/marketing/latest/",
   },
   plugins: [
